1434dfbb6d855e1d69c32ca7096feb377a88eb21
DrPiotrU <piotr.urbanek@p.lodz.pl>
2025-03-24 18:11:22 +0100

    6. Zamiana literałów

--- src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
+++ src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
@@ -2,9 +2,12 @@ package edu.kis.vh.nursery;
 
 public class DefaultCountingOutRhymer {
 
-    private int[] numbers = new int[12];
+    private static final int NUMBERS_COUNT = 12;
+    private static final int EMPTY_INDEX = -1;
 
-    public int total = -1;
+    private int[] numbers = new int[NUMBERS_COUNT];
+
+    public int total = EMPTY_INDEX;
 
     public void countIn(int in) {
         if (!isFull())
@@ -12,22 +15,22 @@ public class DefaultCountingOutRhymer {
     }
 
     public boolean callCheck() {
-        return total == -1;
+        return total == EMPTY_INDEX;
     }
 
     public boolean isFull() {
-        return total == 11;
+        return total == NUMBERS_COUNT - 1;
     }
 
     protected int peekaboo() {
         if (callCheck())
-            return -1;
+            return EMPTY_INDEX;
         return numbers[total];
     }
 
     public int countOut() {
         if (callCheck())
-            return -1;
+            return EMPTY_INDEX;
         return numbers[total--];
     }
 
